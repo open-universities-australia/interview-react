4205cd2ba7f484f83bf9dfe294cbc1a693924a65
Dejan Vasic <dejanvasic24@gmail.com>
2024-03-18 16:05:20 +1100

    Adding level one

---
+++ level-one/src/App.tsx
@@ -0,0 +1,12 @@
+import courses from "./courses.json";
+
+function App() {
+  console.log("courses", courses);
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Courses</h1>
+    </>
+  );
+}
+
+export default App;
